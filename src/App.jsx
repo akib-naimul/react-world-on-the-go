1053a79377f57a898b9f66666b03e5a605d59833
akib-naimul <akibnaimul2000@gmail.com>
2025-09-24 05:39:21 +0600

    module-38 start

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Countries from './components/counties/countries'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Module-38 </h1>
+      <h2>React World On the Go</h2>
+
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
