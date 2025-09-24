b336ef5c7f5f0c0a3b1f9b5d8304de28b27997da
akib-naimul <akibnaimul2000@gmail.com>
2025-09-24 06:31:27 +0600

    grid applied

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country.flags.flags.png)
+    console.log(country)
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name  -  : {country.name.common} </h3>
+            <p>Region :  {country.region.region} </p>
+            <p>Area : {country.area.area} </p>
+            <p>Capital: {country.capital.capital} </p>
+            
         </div>
     );
 };
--- src/components/counties/countries.jsx
+++ src/components/counties/countries.jsx
@@ -1,13 +1,14 @@
 
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
     console.log(countries)
     return (
-        <div>
+        <div className='countries'>
             <h1>Counties List : {countries.length} </h1>
             {
                 countries.map(country => <Country country={country}></Country>)
